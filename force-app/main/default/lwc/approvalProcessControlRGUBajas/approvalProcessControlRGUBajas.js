028175d056fba134ea2531e4f16cdfb09c6328fa
Israel Flores <visraelfs@gmail.com>
2022-03-29 18:25:16 -0600

    Se genera código para que el datatable se preseleccione las referencias que van a pasar a proceso de aprobación

--- force-app/main/default/lwc/approvalProcessControlRGUBajas/approvalProcessControlRGUBajas.js
+++ force-app/main/default/lwc/approvalProcessControlRGUBajas/approvalProcessControlRGUBajas.js
@@ -1,6 +1,7 @@
 import { LightningElement, wire, api, track } from 'lwc';
 import getRelatedReferences from '@salesforce/apex/ApprovalProcessControlRGUController.getRelatedReferences';
 import saveRGUChangeToApprove from '@salesforce/apex/ApprovalProcessControlRGUController.saveRGUChangeToApprove';
+import getRGUToApprove from '@salesforce/apex/ApprovalProcessControlRGUController.getRGUToApprove';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 const columns = [
@@ -20,23 +21,21 @@ const columns = [
 		label: 'Número de sitio',
 		fieldName: 'Numero_Sitio__c',
 		sortable: true,
-		type: 'text',
+		type: 'text'
 	},
 	{
 		label: 'Estatus en CoS',
 		fieldName: 'Status_COS__c',
 		sortable: true,
-		type: 'text',
+		type: 'text'
 	}
-
 ];
-
 
 export default class ApprovalProcessControlRGUBajas extends LightningElement {
 	@api recordId;
 	@track rgus = [];
 	@api kindOfProces = 1;
-	selectedRows;
+	@track selectedRows = [];
 	isLoading = true;
 	columns = columns;
 	defaultSortDirection = 'asc';
@@ -47,23 +46,22 @@ export default class ApprovalProcessControlRGUBajas extends LightningElement {
 		isSaving: false,
 		error: null,
 		data: null
-	}
+	};
 
 	@track toastMessage = {
 		variant: null,
 		title: null,
 		meessage: null
-	}
-
+	};
 
 	//Obtiene las referencias elara del EP asociado al proceso de aprobación
 	@wire(getRelatedReferences, { recordId: '$recordId' })
 	relatedReferences({ data, error }) {
 		if (data) {
 			this.rgus = data;
+			this.selectedRGUToApprove();
 			this.isLoading = false;
-		}
-		else if (error) {
+		} else if (error) {
 			console.log('Error' + JSON.stringify(error));
 		}
 	}
@@ -72,11 +70,11 @@ export default class ApprovalProcessControlRGUBajas extends LightningElement {
 	sortBy(field, reverse, primer) {
 		const key = primer
 			? function (x) {
-				return primer(x[field]);
-			}
+					return primer(x[field]);
+			  }
 			: function (x) {
-				return x[field];
-			};
+					return x[field];
+			  };
 
 		return function (a, b) {
 			a = key(a);
@@ -95,67 +93,111 @@ export default class ApprovalProcessControlRGUBajas extends LightningElement {
 		this.sortedBy = sortedBy;
 	}
 
-	@api
+	//Buscamos si ya hay referencias elara seleccionadas para dar de baja
+	selectedRGUToApprove = function () {
+		getRGUToApprove({ recordId: this.recordId })
+			.then((result) => {
+				let selectedRows =
+					result == null || result === undefined ? [] : result;
 
+				this.selectedRows = selectedRows;
+			})
+			.catch((error) => {
+				this.dataSaving = {
+					isSaving: false,
+					error: error
+				};
+
+				this.toastMessage = {
+					title: 'Error"!',
+					message:
+						'Ocurrio el siguiente error al tratar de guardar los cambios de las referencias elara: ' +
+						error,
+					variant: 'error'
+				};
+
+				const toastMessage = new ShowToastEvent(this.toastMessage);
+				this.dispatchEvent(toastMessage);
+
+				const closeScreen = new CustomEvent('closescreen', {
+					detail: null
+				});
+				this.dispatchEvent(closeScreen);
+			});
+	};
+
+	@api
 	get showSpinner() {
 		return this.isLoading ? true : this.dataSaving.isSaving ? true : false;
 	}
 
-	//Metodo que guarda las referencias para aprobación	
+	//Metodo que guarda las referencias para aprobación
 	handleSave() {
 		var datatable = this.template.querySelector('lightning-datatable');
 		this.selectedRows = datatable.getSelectedRows();
 
 		let ids = '';
 
-		this.selectedRows.forEach(element => {
+		this.selectedRows.forEach((element) => {
 			ids += "'" + element.Id + "',";
 		});
 
 		this.dataSaving = {
 			isSaving: true
-		}
+		};
 
-		saveRGUChangeToApprove({ recordId: this.recordId, idsReferenciasElara: ids }).then(() => {
-			this.dataSaving = {
-				isSaving: false				
-			}			
-
-			this.toastMessage = {
-				title: '¡Listo"!',
-				message: 'Se realizó el guardado de las referencias elara a dar de baja',
-				variant: 'success',
-			}
-
-			const toastMessage = new ShowToastEvent(this.toastMessage);
-			this.dispatchEvent(toastMessage);
-
-			const closeScreen = new CustomEvent("closescreen", { detail: null });
-			this.dispatchEvent(closeScreen);
-
+		saveRGUChangeToApprove({
+			recordId: this.recordId,
+			idsReferenciasElara: ids
 		})
-			.catch(error => {
+			.then(() => {
+				this.dataSaving = {
+					isSaving: false
+				};
+
+				this.toastMessage = {
+					title: '¡Listo"!',
+					message:
+						'Se realizó el guardado de las referencias elara a dar de baja',
+					variant: 'success'
+				};
+
+				const toastMessage = new ShowToastEvent(this.toastMessage);
+				this.dispatchEvent(toastMessage);
+
+				const closeScreen = new CustomEvent('closescreen', {
+					detail: null
+				});
+				this.dispatchEvent(closeScreen);
+				//location.reload();
+				document.dispatchEvent(new CustomEvent('aura://refreshView'));
+			})
+			.catch((error) => {
 				this.dataSaving = {
 					isSaving: false,
 					error: error
-				}
+				};
 
 				this.toastMessage = {
 					title: 'Error"!',
-					message: 'Ocurrio el siguiente error al tratar de guardar los cambios de las referencias elara: ' + error,
-					variant: 'error',
-				}
-	
+					message:
+						'Ocurrio el siguiente error al tratar de guardar los cambios de las referencias elara: ' +
+						error,
+					variant: 'error'
+				};
+
 				const toastMessage = new ShowToastEvent(this.toastMessage);
 				this.dispatchEvent(toastMessage);
-	
-				const closeScreen = new CustomEvent("closescreen", { detail: null });
+
+				const closeScreen = new CustomEvent('closescreen', {
+					detail: null
+				});
 				this.dispatchEvent(closeScreen);
 			});
 	}
 
 	handleCancel() {
-		const closeScreen = new CustomEvent("closescreen", { detail: null });
+		const closeScreen = new CustomEvent('closescreen', { detail: null });
 		this.dispatchEvent(closeScreen);
 	}
 }
--- force-app/main/default/lwc/approvalProcessControlRGUManager/approvalProcessControlRGUManager.js
+++ force-app/main/default/lwc/approvalProcessControlRGUManager/approvalProcessControlRGUManager.js
@@ -1,10 +1,12 @@
 import { LightningElement, api } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
+import { getRecordNotifyChange } from 'lightning/uiRecordApi';
 
 export default class ApprovalProcessControlRGUManager extends LightningElement {
 	@api recordId;
 
-	closeScreen(event){
+	closeScreen(event) {
 		this.dispatchEvent(new CloseActionScreenEvent());
+		location.reload();
 	}
 }
