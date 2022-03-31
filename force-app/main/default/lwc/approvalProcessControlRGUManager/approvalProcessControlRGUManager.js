1725af353ebca726da3ae4e13f59ac209ff4f26e
Israel Flores <visraelfs@gmail.com>
2022-03-31 13:40:37 -0600

    Se genera recurso estatico y se manda a llamar desde el QuickAction

--- force-app/main/default/lwc/approvalProcessControlRGUManager/approvalProcessControlRGUManager.js
+++ force-app/main/default/lwc/approvalProcessControlRGUManager/approvalProcessControlRGUManager.js
@@ -1,8 +1,14 @@
 import { LightningElement, api } from 'lwc';
 import { CloseActionScreenEvent } from 'lightning/actions';
+import { loadStyle } from 'lightning/platformResourceLoader';
+import CustomModal from '@salesforce/resourceUrl/CustomModal';
 
 export default class ApprovalProcessControlRGUManager extends LightningElement {
 	@api recordId;
+
+	connectedCallback() {
+		loadStyle(this, CustomModal);
+	}
 
 	closeScreen(event) {
 		this.dispatchEvent(new CloseActionScreenEvent());
