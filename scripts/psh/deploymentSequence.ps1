4e81730685be94e8563834457317fe7958d8aad4
Israel Flores <visraelfs@gmail.com>
2022-06-07 08:51:38 -0500

    Se realiza deploy en testing exitoso

--- scripts/psh/deploymentSequence.ps1
+++ scripts/psh/deploymentSequence.ps1
@@ -10,4 +10,4 @@ if ( Test-Path -Path $chgSetZip ) {
 
 sfdx force:source:convert -d tmp-package-dir 
 tar.exe -a -c -f changes-set.zip tmp-package-dir
-sfdx force:mdapi:deploy --zipfile changes-set.zip --testlevel RunSpecifiedTests --runtests ApprovalProcessAPITest,ApprovalProcessTriggerSettingsTest,RGU_Change_to_approveAPITest,RGU_Change_to_approveTriggerSettingsTest  -w 3 --checkonly
+sfdx force:mdapi:deploy --zipfile changes-set.zip --testlevel RunSpecifiedTests --runtests ApprovalProcessAPITest,ApprovalProcessTriggerSettingsTest,RGU_Change_to_approveAPITest,RGU_Change_to_approveTriggerSettingsTest  -w 3
